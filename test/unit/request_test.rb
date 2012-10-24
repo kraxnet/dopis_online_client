21c75dcf87c99bafd7f585fc13df3497e993143c
Josef Pospíšil <josef.pospisil@laststar.eu>
2012-10-24 11:50:11 +0200

    Tune test

--- test/unit/request_test.rb
+++ test/unit/request_test.rb
@@ -9,12 +9,12 @@ module DopisOnlineClient
         FakeWeb.register_uri(:post, "https://online3.postservis.cz/cmdedopis/dopisonline.php", :body => File.read(File.join(File.dirname(__FILE__), '../fixtures/success.xml')))
 
         DopisOnlineClient.base_uri 'https://online3.postservis.cz/cmdedopis'
-        DopisOnlineClient.auth('jmeno','heslo')
-        @response = DopisOnlineClient::Request.new(:pdf_file_path=>File.join(File.dirname(__FILE__),'../fixtures/letter.pdf')).deliver
+        DopisOnlineClient.auth('jmeno', 'heslo')
+        @request = DopisOnlineClient::Request.new(:pdf_file_path => File.join(File.dirname(__FILE__), '../fixtures/letter.pdf'))
       end
 
       should "return DopisOnlineClient::Response" do
-        assert_equal true, @response.kind_of?(DopisOnlineClient::Response)
+        assert DopisOnlineClient::Response === @request.deliver
       end
 
     end
